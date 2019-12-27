c55819c1dacf9b602f5232891d2298049733bb68
samuel-lincoln <59297005+samuel-lincoln@users.noreply.github.com>
2019-12-27 18:43:33 -0500

    adds bullet graphics

---
+++ rotr.py
@@ -0,0 +1,8 @@
+# A game of robot survival
+# Each time the player gets to zero time they make it to the next level
+
+# TODO: Make a game screen that's 800 by 600
+# TODO: Make a timer that counts down from 60 seconds to 0
+# TODO: Make player appear on the game board at a random location
+# TODO: Make bullets flY!!!
+
